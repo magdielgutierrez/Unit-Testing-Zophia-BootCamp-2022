aa99e2d913e53b2f2c833da57fd2488eb45a187a
Magdiel Gutiérrez <mgutierreztwp@hotmail.com>
2022-02-05 16:24:28 -0500

    Update unittesting.py

--- scripts/unittesting.py
+++ scripts/unittesting.py
@@ -3,48 +3,48 @@ import unittest
 import json
 
 url_file = open('new_user_info.json', 'r', encoding='utf-8')
-data = json.load(url_file)
+data_dict = json.load(url_file)
 
 
 class MyTestCase(unittest.TestCase):
     def test_type_name(self):
-        self.assertEqual(data['data']['user']['result']['__typename'], 'User')
+        self.assertEqual(data_dict['data']['user']['result']['__typename'], 'User')
 
     def test_size_id(self):
-        self.assertEqual(len(data['data']['user']['result']['id']), 32)
+        self.assertEqual(len(data_dict['data']['user']['result']['id']), 32)
 
     def test_number_rest_id(self):
-        self.assertRegex(data['data']['user']['result']['rest_id'], r'^[0-9]+')
+        self.assertRegex(data_dict['data']['user']['result']['rest_id'], r'^[0-9]+')
 
     def test_none_name(self):
-        self.assertIsNotNone(data['data']['user']['result']['legacy']['name'])
+        self.assertIsNotNone(data_dict['data']['user']['result']['legacy']['name'])
 
     def test_none_screen_name(self):
-        self.assertIsNotNone(data['data']['user']['result']['legacy']['screen_name'])
+        self.assertIsNotNone(data_dict['data']['user']['result']['legacy']['screen_name'])
 
     def test_int_fast_followers_count(self):
-        self.assertIs(type(data['data']['user']['result']['legacy']['fast_followers_count']), int)
+        self.assertIs(type(data_dict['data']['user']['result']['legacy']['fast_followers_count']), int)
 
     def test_int_favourites_count(self):
-        self.assertIs(type(data['data']['user']['result']['legacy']['favourites_count']), int)
+        self.assertIs(type(data_dict['data']['user']['result']['legacy']['favourites_count']), int)
 
     def test_int_followers_count(self):
-        self.assertIs(type(data['data']['user']['result']['legacy']['followers_count']), int)
+        self.assertIs(type(data_dict['data']['user']['result']['legacy']['followers_count']), int)
 
     def test_int_friends_count(self):
-        self.assertIs(type(data['data']['user']['result']['legacy']['friends_count']), int)
+        self.assertIs(type(data_dict['data']['user']['result']['legacy']['friends_count']), int)
 
     def test_int_listed_count(self):
-        self.assertIs(type(data['data']['user']['result']['legacy']['listed_count']), int)
+        self.assertIs(type(data_dict['data']['user']['result']['legacy']['listed_count']), int)
 
     def test_int_media_count(self):
-        self.assertIs(type(data['data']['user']['result']['legacy']['media_count']), int)
+        self.assertIs(type(data_dict['data']['user']['result']['legacy']['media_count']), int)
 
     def test_int_normal_followers_count(self):
-        self.assertIs(type(data['data']['user']['result']['legacy']['normal_followers_count']), int)
+        self.assertIs(type(data_dict['data']['user']['result']['legacy']['normal_followers_count']), int)
 
     def test_int_statuses_count(self):
-        self.assertIs(type(data['data']['user']['result']['legacy']['statuses_count']), int)
+        self.assertIs(type(data_dict['data']['user']['result']['legacy']['statuses_count']), int)
 
 
 if __name__ == '__main__':
